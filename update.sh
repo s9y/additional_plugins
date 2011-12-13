1c3537694e2e46623b12d4bc4903c1426aadd176
Garvin Hicking <blog@garv.in>
2011-12-13 13:27:22 +0100

    syncing

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/garvin/cvs/serendipity/additional_themes
+cvs -q update -d 1>/var/log/s9y-cvs.log 2>&1
+find -name DEADJOE -exec rm {} \;
+cd /home/garvin/cvs/serendipity/additional_plugins
+cvs -q update -d 1>>/var/log/s9y-cvs.log 2>&1
+find -name DEADJOE -exec rm {} \;
+
+/home/garvin/cvs/serendipity/git/additional_plugins/gitclone.sh
+php /home/garvin/cvs/serendipity/additional_plugins/gitclone.php

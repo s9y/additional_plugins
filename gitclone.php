d00a85fa75d70544559e700b66c8385c72dca35e
Garvin Hicking <blog@garv.in>
2011-12-13 13:19:36 +0100

    cloning prep

---
+++ gitclone.php
@@ -0,0 +1,19 @@
+<?php
+chdir(dirname(__FILE__));
+
+$out = explode("\n", `cvs -n -q update`);
+
+foreach($out AS $file) {
+	$file = trim($file);
+	if (empty($file)) continue;
+  
+	if (preg_match('@^\? @', $file)) {
+		$file = str_replace('? ', '', $file);
+		echo "CVS ADD $file\n";
+		`cvs add $file`;
+	} elseif (preg_match('@^U @', $file)) {
+		$file = str_replace('U ', '', $file);
+		echo "CVS RM $file\n";
+		`cvs rm $file`;
+	}
+}
--- gitclone.sh
+++ gitclone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rsync --dry-run \
-      --exclude=.project --exclude=.cvsignore --exclude=SerendipityTranslator.zip --exclude=CVS --exclude=.git --exclude=homepage/by*.htm --exclude=homepage/template*.htm --exclude=homepage/box*.htm --exclude=homepage/last.txt \
-      --archive --no-t --no-p --checksum -i --delete-after -v -z ~/cvs/serendipity/git/_additional_plugins/ ~/cvs/serendipity/additional_plugins   
+      --exclude=/*.zip --exclude=/*.tgz --exclude=.project --exclude=.cvsignore --exclude=SerendipityTranslator.zip --exclude=CVS --exclude=.git --exclude=homepage/by*.htm --exclude=homepage/template*.htm --exclude=homepage/box*.htm --exclude=homepage/last.txt \
+      --archive --no-t --no-p --checksum -i --delete-after -v -z ~/cvs/serendipity/git/additional_plugins/ ~/cvs/serendipity/additional_plugins   
             

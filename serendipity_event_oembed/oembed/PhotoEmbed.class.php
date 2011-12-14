e83110a79540365caea679073eae73e3840eca55
Grischa Brockhaus <github@brockha.us>
2011-12-14 03:22:37 +0100

    Wrong Checkin redone

--- serendipity_event_oembed/oembed/PhotoEmbed.class.php
+++
@@ -1,6 +0,0 @@
-<?
-class PhotoEmbed extends OEmbed{
-    public $url;
-    public $width;
-    public $height;
-}
--- serendipity_event_oembed/oembed/VideoEmbed.class.php
+++
@@ -1,7 +0,0 @@
-<?
-class VideoEmbed extends OEmbed {
-    public $html;
-    public $width;
-    public $height;
-    public $duration; // added by me, not part of OEmbed
-}

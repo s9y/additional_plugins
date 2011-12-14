97ce51559c4ad014c46724847f79aed1717f5eef
Grischa Brockhaus <github@brockha.us>
2011-12-14 03:20:11 +0100

    OEmbed: Autoloading

--- serendipity_event_oembed/oembed/config.php
+++ serendipity_event_oembed/oembed/config.php
@@ -3,7 +3,7 @@ define("PROVIDER_XML",    dirname(__FILE__) . '/' . "providers.xml");
 define("TEMPLATE_PATH",   dirname(__FILE__) . '/' . "templates/");
 
 function __autoload($classname){
-  if(file_exists($x=$classname.".class.php")) {
+  if(file_exists($x=dirname(__FILE__) . '/' .$classname.".class.php")) {
     require_once($x);
   }
 }

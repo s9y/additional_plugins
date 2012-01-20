4e54ddcd6de1a0f8d4d753587de6f49ad33d34a7
Grischa Brockhaus <github@brockha.us>
2012-01-20 21:53:30 +0100

    commentSpice: utf8 language file

--- serendipity_event_commentspice/UTF-8/lang_de.inc.php
+++ serendipity_event_commentspice/UTF-8/lang_de.inc.php
@@ -1,6 +1,6 @@
 <?php
 @define('PLUGIN_EVENT_COMMENTSPICE_TITLE', 'Comment Spice');
-@define('PLUGIN_EVENT_COMMENTSPICE_DESC',  'W¸rze Deinen Kommentarbereich mit Extras wie einem Twitterlink oder einem Link auf den letzten Artikel des Kommentators.');
+@define('PLUGIN_EVENT_COMMENTSPICE_DESC',  'W√ºrze Deinen Kommentarbereich mit Extras wie einem Twitterlink oder einem Link auf den letzten Artikel des Kommentators.');
 @define('PLUGIN_EVENT_COMMENTSPICE_EXPERIMENTAL', 'comment spice experimentell');
 
 @define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_TWITTER', 'Auf Twitter lesen');
@@ -8,5 +8,5 @@
 
 @define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_CHOOSE', '- Bewirb einen Deiner letzten Artikel -');
 @define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_RESCENT', '%s schrieb auch');
-@define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_FOOTER', '<b>Bewirb einen Deiner letzten Artikel</b><br/>Dieses Blog erlaubt Dir mit Deinem Kommentar einen Deiner letzten Artikel zu bewerben. Bitte gib Deine Blog URL als Homepage ein, dann wird eine Auswahl erscheinen, in der Du einen Artikel ausw‰hlen kannst.'); 
-@define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_CORRUPTED', 'Entschuldigung, bei der ‹bergabe "Deines letzten Artikels" ist etwas schief gegangen.');
+@define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_FOOTER', '<b>Bewirb einen Deiner letzten Artikel</b><br/>Dieses Blog erlaubt Dir mit Deinem Kommentar einen Deiner letzten Artikel zu bewerben. Bitte gib Deine Blog URL als Homepage ein, dann wird eine Auswahl erscheinen, in der Du einen Artikel ausw√§hlen kannst.'); 
+@define('PLUGIN_EVENT_COMMENTSPICE_PROMOTE_ARTICLE_CORRUPTED', 'Entschuldigung, bei der √úbergabe "Deines letzten Artikels" ist etwas schief gegangen.');

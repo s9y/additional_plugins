a830a66bcf30d1c701591f1766e6458f8f2f2caa
Ian <aristophian@googlemail.com>
2012-02-10 11:51:48 +0100

    constant was hidden :)

--- serendipity_event_entrycheck/UTF-8/lang_de.inc.php
+++ serendipity_event_entrycheck/UTF-8/lang_de.inc.php
@@ -13,7 +13,7 @@
         @define('PLUGIN_EVENT_ENTRYCHECK_EMPTYBODY_WARNING', 'Es ist nicht erlaubt einen Artikel mit leerem "Eintrag" zu veröffentlichen. Bitte einen Text in das Feld "Eintrag" eingeben und erneut speichern!');
         @define('PLUGIN_EVENT_ENTRYCHECK_EMPTYEXTENDED', 'Leeren erweiterten Eintrag verhindern?');
         @define('PLUGIN_EVENT_ENTRYCHECK_EMPTYEXTENDED_DESC', 'Wenn auf "ja" gesetzt, muss ein Artikel Text im Feld "Erweiterter Eintrag" enthalten.');
-        @define('PLUGIN_EVENT_ENTRYCHECK_EMPTYEXTENDED_WARNING', 'Es ist nicht erlaubt einen Eintrag mit leerem "Erweiterter Eintrag" zu veröffentlichen. Bitte einen Text in das Feld "Erweiterter Eintrag" eingeben und erneut speichern!');        @define('PLUGIN_EVENT_ENTRYCHECK_DEFAULTCAT', 'Standardkategorie definieren');
+        @define('PLUGIN_EVENT_ENTRYCHECK_EMPTYEXTENDED_WARNING', 'Es ist nicht erlaubt einen Eintrag mit leerem "Erweiterter Eintrag" zu veröffentlichen. Bitte einen Text in das Feld "Erweiterter Eintrag" eingeben und erneut speichern!');
         @define('PLUGIN_EVENT_ENTRYCHECK_DEFAULTCAT', 'Standardkategorie definieren');
         @define('PLUGIN_EVENT_ENTRYCHECK_DEFAULTCAT_DESC', 'Falls der Benutzer eine Kategorie leergelassen hat, kann der Eintrag automatisch dieser angegebenen Kategorie zugewiesen werden.');
 

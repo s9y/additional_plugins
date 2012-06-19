19c130c8cfca029cecaafde9a89ff749b2009b2e
Grischa Brockhaus <github@brockha.us>
2012-06-20 01:28:14 +0200

    ESTRICT "static" patch

--- serendipity_event_xmlrpc/PEAR/XML/RPC.php
+++ serendipity_event_xmlrpc/PEAR/XML/RPC.php
@@ -1388,7 +1388,7 @@ class XML_RPC_Message extends XML_RPC_Base
      * @link   http://php.net/xml_parser_create
      * @since  Method available since Release 1.2.0
      */
-    function getEncoding($data)
+    static function getEncoding($data)
     {
         global $XML_RPC_defencoding;
 

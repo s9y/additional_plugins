1c0dd9b84c764be739b0742bfa521d1630f729a3
ogerlach <info@stumblingpilgrim.net>
2013-11-19 22:08:22 +0100

    1. Updated Englisch language file to reflect the changes in plugin code. 2. Added a German language file.

---
+++ serendipity_plugin_sidebarlogo/UTF-8/lang_de.inc.php
@@ -0,0 +1,29 @@
+<?php # 
+
+/**
+ *  @version 0.3
+ *  @author Oliver Gerlach <programmer@stumblingpilgrim.net>
+ *  EN-Revision: Revision of lang_en.inc.php
+ */
+
+@define('PLUGIN_SIDEBARLOGO_NAME', 'SideBar Logo 0.3');
+@define('PLUGIN_SIDEBARLOGO_DESC', 'Zeigt ein Logo mit einer Beschreibung und allen relevanten Informationen in der Seitenleiste.');
+@define('PLUGIN_SIDEBARLOGO_AUTH', 'Adam Krause');
+@define('PLUGIN_SIDEBARLOGO_TITLE', 'Titel');
+@define('PLUGIN_SIDEBARLOGO_TITLE_DESC', 'Titeltext für das Seitenleistenelement');
+@define('PLUGIN_SIDEBARLOGO_IMAGE', 'Logo');
+@define('PLUGIN_SIDEBARLOGO_IMAGE_DESC', 'Bild aus der Medienbibliothek, das neben der Beschreibung angezeigt werden soll. Leer lassen, um kein Bild anzuzeigen.');
+@define('PLUGIN_SIDEBARLOGO_IMAGEWIDTH', 'Bildbreite');
+@define('PLUGIN_SIDEBARLOGO_IMAGEWIDTH_DESC', 'Bildbreite des Logos (CSS-Definitionen wie 50px or 200% verwewnden oder leer lassen');
+@define('PLUGIN_SIDEBARLOGO_IMAGEHEIGHT', 'Bildhöhe');
+@define('PLUGIN_SIDEBARLOGO_IMAGEHEIGHT_DESC', 'Bildhöhe des Logos (CSS-Definitionen wie 50px or 200% verwewnden oder leer lassen');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT', 'Bildbeschreibung');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT_DESC', 'beschreibender Text für das Bild (erforderlich)');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT_MISSING', 'keine Beschreibung');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTION', 'Beschreibung');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTION_DESC', 'Beschreibung für das Blog oder was auch immer im Element angezeigt werden soll. HTML kann für weitere Formatierung verwendet werden.');
+@define('PLUGIN_SIDEBARLOGO_IMAGESTYLE', 'Bildstyle');
+@define('PLUGIN_SIDEBARLOGO_IMAGESTYLE_DESC', 'Style, der für das Bild verwendet werden soll. Mit dem Zeichen "." beginnen, um eine CSS-ID zu spezifizieren, mit "#" beginnen, um eine Klasse zu spezifizieren oder alternativ einen Inline-Style direkt eingeben');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTIONSTYLE', 'Beschreibungstyle');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTIONSTYLE_DESC', 'Style, der für die Beschreibung verwenden werden soll. to use for your description.  Mit dem Zeichen "." beginnen, um eine CSS-ID zu spezifizieren, mit "#" beginnen, um eine Klasse zu spezifizieren, direkt Inline-Style definieren oder frei lassen, um das Standardseitenleistenstyling zu verwenden (empfohlen)');
+?>
--- serendipity_plugin_sidebarlogo/lang_en.inc.php
+++ serendipity_plugin_sidebarlogo/lang_en.inc.php
@@ -1,34 +1,29 @@
 <?php # 
 
 /**
- *  @version 0.0.1
- *  @author Translator Name <webmaster@oxygenws.com>
+ *  @version 0.3
+ *  @author Oliver Gerlach <programmer@stumblingpilgrim.net>
  *  EN-Revision: Revision of lang_en.inc.php
  */
 
-@define('PLUGIN_SIDEBARLOGO_NAME', 'SideBar Logo 0.0.2');
-@define('PLUGIN_SIDEBARLOGO_DESC', 'Displays a panel containing your logo, site name, and other relevant informaion about your site.');
+@define('PLUGIN_SIDEBARLOGO_NAME', 'SideBar Logo 0.3');
+@define('PLUGIN_SIDEBARLOGO_DESC', 'Displays a panel containing your logo with a description including all relevant information about your site.');
 @define('PLUGIN_SIDEBARLOGO_AUTH', 'Adam Krause');
 @define('PLUGIN_SIDEBARLOGO_TITLE', 'Title');
-@define('PLUGIN_SIDEBARLOGO_TITLE_DESC', 'The Text that displays in the SideBar Logo panel');
-@define('PLUGIN_SIDEBARLOGO_SITENAME', 'Name');
-@define('PLUGIN_SIDEBARLOGO_SITENAME_DESC', 'The name of your web site (Volkswagen of America)');
-@define('PLUGIN_SIDEBARLOGO_SITETAG', 'Tag Line');
-@define('PLUGIN_SIDEBARLOGO_SITETAG_DESC', 'A slogan or tag line (Autobahn for All)');
+@define('PLUGIN_SIDEBARLOGO_TITLE_DESC', 'Text for sidebar item title bar');
 @define('PLUGIN_SIDEBARLOGO_IMAGE', 'Logo');
-@define('PLUGIN_SIDEBARLOGO_IMAGE_DESC', 'Select an image from your Serendipity Media Library');
+@define('PLUGIN_SIDEBARLOGO_IMAGE_DESC', 'image from media library to display in content section or leave blank for no image');
 @define('PLUGIN_SIDEBARLOGO_IMAGEWIDTH', 'Image Width');
-@define('PLUGIN_SIDEBARLOGO_IMAGEWIDTH_DESC', 'Constrain logo width (leave blank for no constraint, 50px, or 200%)');
+@define('PLUGIN_SIDEBARLOGO_IMAGEWIDTH_DESC', 'display width of logo (use CSS definitions like 50px or 200% or leave blank for no constraint)');
 @define('PLUGIN_SIDEBARLOGO_IMAGEHEIGHT', 'Image Height');
-@define('PLUGIN_SIDEBARLOGO_IMAGEHEIGHT_DESC', 'Constrain logo height (leave blank for no constraint, 50px or 200%)');
-@define('PLUGIN_SIDEBARLOGO_DESCRIPTION', 'Description');
-@define('PLUGIN_SIDEBARLOGO_DESCRIPTION_DESC', 'Describes the purpose of your site');
-@define('PLUGIN_SIDEBARLOGO_CONTACT', 'Contact');
-@define('PLUGIN_SIDEBARLOGO_CONTACT_DESC', 'An email address or mailto link ("Contact me: <a href=mailto:john@xyz.com>John</a>")');
-@define('PLUGIN_SIDEBARLOGO_COPYRIGHT', 'Copyright');
-@define('PLUGIN_SIDEBARLOGO_COPYRIGHT_DESC', 'Tiny text, best used for copyright or legal statement (copyright symbol: "&copy;")');
-@define('PLUGIN_SIDEBARLOGO_STYLE', 'Content Styling');
-@define('PLUGIN_SIDEBARLOGO_STYLE_DESC', 'Style your panel content (clear the field and save 2X to reload defaults)');
-@define('PLUGIN_SIDEBARLOGO_SEQUENCE', 'Sequence');
-@define('PLUGIN_SIDEBARLOGO_SEQUENCE_DESC', 'Define the sequence of your SideBar Logo elements');
-
+@define('PLUGIN_SIDEBARLOGO_IMAGEHEIGHT_DESC', 'display height of logo (use CSS definitions like 50px or 200% or leave blank for no constraint)');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT', 'Image Text');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT_DESC', 'descriptive text for image (required)');
+@define('PLUGIN_SIDEBARLOGO_IMAGETEXT_MISSING', 'no description');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTION', 'Description text');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTION_DESC', 'Descriptive text for your blog or whatever you want to write. HTML can be used for further formatting.');
+@define('PLUGIN_SIDEBARLOGO_IMAGESTYLE', 'Image Style');
+@define('PLUGIN_SIDEBARLOGO_IMAGESTYLE_DESC', 'style to use for your logo. Start text with "." to provide a CSS id, with a "#" to provide a class or use inline styling directly');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTIONSTYLE', 'Description Style');
+@define('PLUGIN_SIDEBARLOGO_DESCRIPTIONSTYLE_DESC', 'style to use for your description. Start text with "." to provide a CSS id, with a "#" to provide a class, use direct styling or leave it blank to use default sidebar styling (recommended)');
+?>

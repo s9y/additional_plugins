a4114b6283e4ff60ee901f47f0c77a60b2bc4030
Ian <aristophian@googlemail.com>
2014-05-28 20:08:30 +0200

    upgrade to ckeditor plugin version 4.1.0

--- serendipity_event_ckeditor/cke_config.js
+++ serendipity_event_ckeditor/cke_config.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * @fileOverview A Serendipity serendipity_event_ckeditor CKEDITOR custom config file: cke_config.js, v. 1.7, 2014-03-30
+ * @fileOverview A Serendipity serendipity_event_ckeditor CKEDITOR custom config file: cke_config.js, v. 1.8, 2014-05-28
  */
 
 /**
@@ -21,43 +21,46 @@ CKEDITOR.editorConfig = function( config ) {
         config.filebrowserFlashUploadUrl = CKEDITOR_BASEPATH +'../kcfinder/upload.php?type=flash';
     }
 
-    // Set ACF by serendipity_event_ckeditor plugin option - default (false)
-    // The automatic mode is on (false) when the CKEDITOR.config.allowedContent option is not set in your editor configuration. 
-    // This is the default setting which means that from now on by default all CKEditor contents will be filtered.
     // Advanced Content Filter works in two modes:
-    //   automatic – the filter is configured by editor features (like plugins, buttons, and commands) that are enabled with configuration options 
-    //               such as CKEDITOR.config.plugins, CKEDITOR.config.extraPlugins, and CKEDITOR.config.toolbar,
-    //   custom    – the filter is configured by the CKEDITOR.config.allowedContent option and only features that match this setting are activated.
+    //      automatic – the filter is configured by editor features (like plugins, buttons, and commands) that are enabled with configuration options
+    //                  such as CKEDITOR.config.plugins, CKEDITOR.config.extraPlugins, and CKEDITOR.config.toolbar,
+    //      custom    – the filter is configured by the CKEDITOR.config.allowedContent option and only features that match this setting are activated.
     // In both modes it is possible to extend the filter configuration by using the CKEDITOR.config.extraAllowedContent setting.
-    // If you want to disable Advanced Content Filter, set CKEDITOR.config.allowedContent to true. All available editor features will be activated and input data will not be filtered. 
+    // If you want to disable Advanced Content Filter, set CKEDITOR.config.allowedContent to true.
+    // All available editor features will be activated and input data will not be filtered.
     // Allowed content rules. This setting is used when instantiating CKEDITOR.editor.filter.
     // The following values are accepted:
-    //     CKEDITOR.filter.allowedContentRules – defined rules will be added to the CKEDITOR.editor.filter.
-    //     true – will disable the filter (data will not be filtered, all features will be activated).
-    //     default – the filter will be configured by loaded features (toolbar items, commands, etc.).
-    // In all cases filter configuration may be extended by extraAllowedContent. This option may be especially useful when you want to use the default allowedContent value along with some additional rules.
+    //      CKEDITOR.filter.allowedContentRules – defined rules will be added to the CKEDITOR.editor.filter.
+    //      true – will disable the filter (data will not be filtered, all features will be activated).
+    //      default – the filter will be configured by loaded features (toolbar items, commands, etc.).
+    // In all cases filter configuration may be extended by extraAllowedContent. This option may be especially useful,
+    // when you want to use the default allowedContent value along with some additional rules.
     //
+    // List of regular expressions to be executed on ***input HTML***, indicating HTML source code that, when matched, must not be available in the WYSIWYG mode for editing.
+
+    // Set ACF by serendipity_event_ckeditor plugin option - default (false)
+    // The automatic mode is on (false) when the CKEDITOR.config.allowedContent option is not set in your editor configuration.
+    // This is the default setting which means that from now on by default all CKEditor contents will be filtered.
     if (CONFIG_ACF_OFF === true) {
         config.allowedContent = CONFIG_ACF_OFF;
     } else { // this is ACF ON by default
 
-        // List of regular expressions to be executed on ***input HTML***, indicating HTML source code that, when matched, must not be available in the WYSIWYG mode for editing. 
+        // List of regular expressions to be executed on ***input HTML***, indicating HTML source code that, when matched, must not be available in the WYSIWYG mode for editing.
 
         // allow <script> tags
-        config.protectedSource.push( /<(script)[^>]*>.*<\/script>/ig ); // set default in ckeditor.js [/<script[\s\S]*?<\/script>/gi,/<noscript[\s\S]*?<\/noscript>/gi]
-        // allow imageselectorplus mediainsert tag code
+        //config.protectedSource.push( /<(script)[^>]*>.*<\/script>/ig ); // already set as default in ckeditor.js [/<script[\s\S]*?<\/script>/gi,/<noscript[\s\S]*?<\/noscript>/gi]
+        // allow imageselectorplus mediainsert tag codes
         config.protectedSource.push( /<(mediainsert)[^>]*>[\s\S]*?<\/mediainsert>/img );
-        // allow multiline HTML5 audio tags
-        //config.protectedSource.push( /<(audio)[^>]*>[\s\S]*?<\/audio>/img ); //looks like we dont need this any more and placeholding replaces bad now switching mode
-        // allow a Smarty like {} tag syntax without starting whitespace, which would be some other code part.
+        // allow a Smarty like {} tag syntax without inner whitespace, which would be some other code part.
         config.protectedSource.push( /\{[a-zA-Z\$].*?\}/gi );
+        // allow wp like [[mytag]] tags with >= 4.4.1
+        config.protectedSource.push(/\[\[[^\]]*?\]\]/g);
 
         // Set placeholder tag cases - elements [attributes]{styles}(classes)
         // Allowed mediainsert, gallery, media tags (imageselectorplus galleries) - which tells ACF to not touch the code!
-        // Allowed audio/div tag attributes, styles and classes - was a need for Media Library inserts, which tells ACF to not touch the code!
-        // Allowed further p/span tag styles and/or classes, since commonly used by authors  - which tells ACF to not touch the code!
+        // Allowed div is a need for Media Library inserts - which tells ACF to not touch the code!
         // img[height] is even needed to avoid ACF OFF removement of height attributes
-        config.extraAllowedContent = 'mediainsert;gallery[*];media[*];script[*];audio[*](*);div[*]{*}(*);p{*}(*);span{*}(*);img[height,width];';
+        config.extraAllowedContent = 'mediainsert[*]{*}(*);gallery[*]{*}(*);media[*]{*}(*);script[*]{*}(*);audio[*]{*}(*);div[*]{*}(*);img[height,width];';
 
         // CKEDITOR.protectedSource patterns used regex Escape sequences
         //            \s any whitespace character; 
@@ -88,6 +91,21 @@ CKEDITOR.editorConfig = function( config ) {
     // You can always use SHIFT+ENTER to set a br in the P-mode default option or change the SHIFT-mode to something else
     //config.shiftEnterMode = CKEDITOR.ENTER_BR;
 
+    // Whether to use HTML entities in the output. Default Value: true
+    //config.entities = false;
+
+    //config.htmlEncodeOutput = false;
+
+    // The configuration setting that controls the ENTER mode is "config.enterMode" and it offers three options:
+    // (1) The default creates a paragraph element each time the "enter" key is pressed:
+    //config.enterMode = CKEDITOR.ENTER_P; // inserts <p></p>
+    // (2) You can choose to create a "div" element instead of a paragraph:
+    //config.enterMode = CKEDITOR.ENTER_DIV; // inserts <div></div>
+    // (3) If you prefer to not wrap the text in anything, you can choose to insert a line break tag:
+    //config.enterMode = CKEDITOR.ENTER_BR; // inserts <br />
+    // You can always use SHIFT+ENTER to set a br in the P-mode default option or change the SHIFT-mode to something else
+    //config.shiftEnterMode = CKEDITOR.ENTER_BR;
+
     //config.entities = false;
     //config.htmlEncodeOutput = false;
 
@@ -106,9 +124,24 @@ CKEDITOR.editorConfig = function( config ) {
     config['skin'] = 'moono';
     config['height'] = 400;
 
+    // Allow certain font sizes, eg.
+    //config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;15/15px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px' ;
+    // Allow one(!) default font label, eg.
+    //config.font_defaultLabel = 'Arial';
+    // Add other font names to the list of fonts names to be displayed in the Font combo in the toolbar. - eg.
+    //config.font_names = config.font_names + 
+    //    'Arial/Arial, Helvetica, sans-serif;' +
+    //    'Times New Roman/Times New Roman, Times, serif;' +
+    //    'Verdana';
+
+    // Add custom Serendipity styles to ckeditor wysiwyg-mode, to repect S9y css image floats
+    config.contentsCss = CKEDITOR_PLUGPATH + 'serendipity_event_ckeditor/wysiwyg-style.css';
+
     // remove custom toolbar buttons and plugins
     //config.removePlugins = 'flash,iframe';
-    config.removeButtons = 'Styles';
+    config.removeButtons = 'Styles'; // this button is quite useless in a blog and the toolbar Groups break better on screens
+
+    config.toolbar_Simple = [['Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat']];
 
     // set the serendipity_event_ckeditor custom toolbar group
     // Note: indent is disabled, mediaembed and pbckcode plugins are set here, and procurator placeholders for "protected Source" is buttonless
@@ -130,5 +163,8 @@ CKEDITOR.editorConfig = function( config ) {
         { name: 'about' },
         { name: 'cheatsheet' }
     ];
+
+    // A list of plugins that must not be loaded. This setting makes it possible to avoid loading some plugins defined in the CKEDITOR.config.plugins setting, without having to touch it and potentially break it.
+    //config.removePlugins = 'elementspath,save,font';
 
 };

49ef7d5cdefe18faa176a288a9364744ebe8dad3
Ian <aristophian@googlemail.com>
2014-06-23 17:43:55 +0200

    fix old lang typo

--- serendipity_event_imageselectorplus/UTF-8/lang_de.inc.php
+++ serendipity_event_imageselectorplus/UTF-8/lang_de.inc.php
@@ -7,6 +7,6 @@
 @define('PLUGIN_EVENT_IMAGESELECTORPLUS_TARGET_ENTRY', 'Isolierter Eintrag');
 @define('PLUGIN_EVENT_IMAGESELECTORPLUS_TARGET_BLANK', 'Popup (via target=_blank)');
 @define('PLUGIN_EVENT_IMAGESELECTORPLUS_QUICKBLOG', 'QuickBlog');
-@define('PLUGIN_EVENT_IMAGESELECTORPLUS_QUICKBLOG_DESC', 'Wenn Sie bei den folgenden Feldern mindestens einen Titel eintragen, wir das Bild sofort als neuer Blog-Artikel eingestellt. Das Ausgabedesign kann über die Datei quickblog.tpl eingestellt werden.');
+@define('PLUGIN_EVENT_IMAGESELECTORPLUS_QUICKBLOG_DESC', 'Wenn Sie bei den folgenden Feldern mindestens einen Titel eintragen, wird das Bild sofort als neuer Blog-Artikel eingestellt. Das Ausgabedesign kann über die Datei quickblog.tpl eingestellt werden.');
 
 @define('PLUGIN_EVENT_IMAGESELECTORPLUS_IMAGE_SIZE_DESC', 'Wenn Sie die voreingestellte $serendipity[\'thumbSize\'] Größe hier ändern, wird ein zusätzliches Bild in der genannten Größe in der Mediendatenbank erstellt. Dies Image Instanz wird dann im Frontend als Bildvorschau mit entsprechendem Link zum Original Bild in ihrem Blogeintrag benutzt.');

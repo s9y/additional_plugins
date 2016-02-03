8c08cc0875ef01d5f444b4cc0336ceab63e775ae
Ian <aristophian@googlemail.com>
2016-02-03 11:20:12 +0100

    remove jquery plugin

--- serendipity_event_jquery/lang_cs.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- *  @author Vladimír Ajgl <vlada@ajgl.cz>
- *  @translated 2009/05/18
- */
-
-@define('EVENT_JQUERY_TITLE', 'Implementace jQuery');
-@define('EVENT_JQUERY_DESC', 'Umožòuje ostatnám pluginùm používat jQuery');
--- serendipity_event_jquery/lang_cz.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- *  @author Vladimír Ajgl <vlada@ajgl.cz>
- *  @translated 2009/05/18
- */
-
-@define('EVENT_JQUERY_TITLE', 'Implementace jQuery');
-@define('EVENT_JQUERY_DESC', 'Umo¾òuje ostatnám pluginùm pou¾ívat jQuery');

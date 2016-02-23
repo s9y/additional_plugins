182bee9d856b15c3c56caddb621040e94a2bc7f6
Garvin Hicking <blog@garv.in>
2016-02-23 09:38:28 +0100

    debug

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+echo "WHOAMI: `whoami`";
 cd /home/garvin/cvs/serendipity/additional_themes
-cvs -q update -d 1>/var/log/s9y-cvs.log 2>&1
+echo "CVS update additional_themes:";
+cvs -v update -d 1>/var/log/s9y-cvs.log 2>&1
 find -name DEADJOE -exec rm {} \;
 cd /home/garvin/cvs/serendipity/additional_plugins
-cvs -q update -d 1>>/var/log/s9y-cvs.log 2>&1
+echo "CVS update additional_plugins:";
+cvs -v update -d 1>>/var/log/s9y-cvs.log 2>&1
 find -name DEADJOE -exec rm {} \;
 
+echo "Gitclone.sh:";
 /home/garvin/cvs/serendipity/git/additional_plugins/gitclone.sh
+echo "Gitclone.php additional_plugins:";
 php /home/garvin/cvs/serendipity/additional_plugins/gitclone.php
+echo "Gitclone.php additional_themes:";
 php /home/garvin/cvs/serendipity/additional_themes/gitclone.php

52ccfd416be8a107e609f4f113f1c9944f75d998
Garvin Hicking <blog@garv.in>
2016-04-26 17:06:30 +0200

    unstash

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 echo "WHOAMI: `whoami`";
 cd /home/garvin/cvs/serendipity/additional_themes
 echo "CVS update additional_themes:";
-cvs -v update -d 1>/var/log/s9y-cvs.log 2>&1
+cvs update -d 1>/var/log/s9y-cvs.log 2>&1
 find -name DEADJOE -exec rm {} \;
 cd /home/garvin/cvs/serendipity/additional_plugins
 echo "CVS update additional_plugins:";
-cvs -v update -d 1>>/var/log/s9y-cvs.log 2>&1
+cvs update -d 1>>/var/log/s9y-cvs.log 2>&1
 find -name DEADJOE -exec rm {} \;
 
 echo "Gitclone.sh:";

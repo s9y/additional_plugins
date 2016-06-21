eff18b2d9b1424a4845415a4280017c0c3046f00
Matthias Mees <mm@yellowled.de>
2016-06-21 17:34:49 +0200

    Fix some typos

--- serendipity_event_social/UTF-8/lang_en.inc.php
+++ serendipity_event_social/UTF-8/lang_en.inc.php
@@ -1,7 +1,7 @@
 <?php #
 
-@define('PLUGIN_EVENT_SOCIAL_NAME', 'Share Buttons');
-@define('PLUGIN_EVENT_SOCIAL_DESC', 'Show social share buttons for services like Facebook and Twitter, using shariff.');
+@define('PLUGIN_EVENT_SOCIAL_NAME', 'Share buttons');
+@define('PLUGIN_EVENT_SOCIAL_DESC', 'Show social share buttons for services like Facebook and Twitter using shariff.');
 @define('PLUGIN_EVENT_SOCIAL_SERVICES', 'Available Services');
 @define('PLUGIN_EVENT_SOCIAL_SERVICES_DESC', 'Select the services for which you want to show a share button.');
 @define('PLUGIN_EVENT_SOCIAL_THEME', 'Color theme');
@@ -9,5 +9,5 @@
 @define('PLUGIN_EVENT_SOCIAL_THEME_STD', 'Standard');
 @define('PLUGIN_EVENT_SOCIAL_THEME_WHITE', 'White');
 @define('PLUGIN_EVENT_SOCIAL_THEME_GREY', 'Grey');
-@define('PLUGIN_EVENT_SOCIAL_OVERVIEW', 'Show in overview pages.');
-@define('PLUGIN_EVENT_SOCIAL_OVERVIEW_DESC', 'Also show the share buttons on overview pages, like the frontapge.');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW', 'Show on overview pages');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW_DESC', 'Also show the share buttons on overview pages (like i.e. the front page.');

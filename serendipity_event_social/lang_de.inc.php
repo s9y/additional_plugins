97682b17bed4927e277d900d7fa17924e4b54a3b
Matthias Mees <mm@yellowled.de>
2016-06-22 10:56:08 +0200

    Add latin1 lang files

---
+++ serendipity_event_social/lang_de.inc.php
@@ -0,0 +1,13 @@
+<?php #
+
+@define('PLUGIN_EVENT_SOCIAL_NAME', 'Share-Buttons');
+@define('PLUGIN_EVENT_SOCIAL_DESC', 'Zeige Share-Buttons für Netzwerke wie Facebook und Twitter (nutzt shariff)');
+@define('PLUGIN_EVENT_SOCIAL_SERVICES', 'Unterstützte Netzwerke');
+@define('PLUGIN_EVENT_SOCIAL_SERVICES_DESC', 'Wähle die Netzwerke, für die ein Share-Button angezeigt werden soll.');
+@define('PLUGIN_EVENT_SOCIAL_THEME', 'Farbschema');
+@define('PLUGIN_EVENT_SOCIAL_THEME_DESC', 'Wähle ein Farbschema für die Share-Buttons.');
+@define('PLUGIN_EVENT_SOCIAL_THEME_STD', 'Standard');
+@define('PLUGIN_EVENT_SOCIAL_THEME_WHITE', 'Weiß');
+@define('PLUGIN_EVENT_SOCIAL_THEME_GREY', 'Grau');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW', 'Auf Übersichtsseiten anzeigen');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW_DESC', 'Zeige die Share-Buttons auch auf Übersichtsseiten (wie z.B. der Startseite).');
---
+++ serendipity_event_social/lang_en.inc.php
@@ -0,0 +1,13 @@
+<?php #
+
+@define('PLUGIN_EVENT_SOCIAL_NAME', 'Share buttons');
+@define('PLUGIN_EVENT_SOCIAL_DESC', 'Show social share buttons for services like Facebook and Twitter using shariff.');
+@define('PLUGIN_EVENT_SOCIAL_SERVICES', 'Available Services');
+@define('PLUGIN_EVENT_SOCIAL_SERVICES_DESC', 'Select the services for which you want to show a share button.');
+@define('PLUGIN_EVENT_SOCIAL_THEME', 'Color theme');
+@define('PLUGIN_EVENT_SOCIAL_THEME_DESC', 'Select a color theme for the share buttons.');
+@define('PLUGIN_EVENT_SOCIAL_THEME_STD', 'Standard');
+@define('PLUGIN_EVENT_SOCIAL_THEME_WHITE', 'White');
+@define('PLUGIN_EVENT_SOCIAL_THEME_GREY', 'Grey');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW', 'Show on overview pages');
+@define('PLUGIN_EVENT_SOCIAL_OVERVIEW_DESC', 'Also show the share buttons on overview pages (like i.e. the front page.');

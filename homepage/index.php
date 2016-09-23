93bb840a8bb12b195e21ac36984f691c90868ba3
Matthias Mees <mm@yellowled.de>
2016-09-23 09:19:24 +0200

    New CSS/JS build

--- homepage/index.php
+++ homepage/index.php
@@ -188,7 +188,7 @@ if (is_dir('homepage')) {
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script>window.jQuery || document.write('<script src="/scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="/scripts/master.js"></script>
     <!-- Google Analytics -->

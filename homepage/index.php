84824b913e37918d18960db83a61a6bc3f83ddad
Matthias Mees <mm@yellowled.de>
2016-09-27 10:07:48 +0200

    Fix path to scripts on spartacus

--- homepage/index.php
+++ homepage/index.php
@@ -189,8 +189,8 @@ if (is_dir('homepage')) {
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/scripts/jquery/dist/jquery.min.js"><\/script>')</script>
-    <script src="/scripts/master.js"></script>
+    <script>window.jQuery || document.write('<script src="scripts/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="scripts/master.js"></script>
     <!-- Google Analytics -->
     <script>
     window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;

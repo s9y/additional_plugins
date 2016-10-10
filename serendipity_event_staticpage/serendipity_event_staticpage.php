40396371d26e0842dc5fbc298f0b279c3ea2e43a
Garvin Hicking <blog@garv.in>
2016-10-10 12:07:02 +0200

    bump version and required version

--- serendipity_event_staticpage/serendipity_event_staticpage.php
+++ serendipity_event_staticpage/serendipity_event_staticpage.php
@@ -91,9 +91,9 @@ class serendipity_event_staticpage extends serendipity_event
         $propbag->add('page_configuration', $this->config);
         $propbag->add('type_configuration', $this->config_types);
         $propbag->add('author', 'Marco Rinck, Garvin Hicking, David Rolston, Falk Doering, Stephan Manske, Pascal Uhlmann, Ian, Don Chambers');
-        $propbag->add('version', '4.10');
+        $propbag->add('version', '4.11');
         $propbag->add('requirements',  array(
-            'serendipity' => '1.3',
+            'serendipity' => '2.0',
             'smarty'      => '2.6.7',
             'php'         => '4.1.0'
         ));

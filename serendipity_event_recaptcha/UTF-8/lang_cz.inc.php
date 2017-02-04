5faa85fc41449787cead9c1b35b71da40403695b
Thomas Hochstein <thh@inter.net>
2017-02-05 00:15:09 +0100

    Update reCAPTCHA documentation.
    recaptcha.net is gone and replaced by
    google.com/recaptcha/. Change URLs accordingly.
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_recaptcha/UTF-8/lang_cz.inc.php
+++ serendipity_event_recaptcha/UTF-8/lang_cz.inc.php
@@ -15,16 +15,16 @@
 
 
 @define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA', 'Použít kryptogramy Recaptcha');
-@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_DESC', 'Pokud je nastaveno, budou použity kryptogramy Recaptcha. To je speciální druh kryptogramů, který pomáhá při digitalizaci knih. Viz http://www.recaptcha.net. Uživatel si může vybrat, že místo zadávání zobrazených písmen mu bude přehrána krátká zpráva obsahující čísla, která slouží jako kód. Pokud nejsou generovány žádné kryptogramy, server je pravděpodobně mimo službu.');
+@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_DESC', 'Pokud je nastaveno, budou použity kryptogramy Recaptcha. To je speciální druh kryptogramů, který pomáhá při digitalizaci knih. Viz https://www.google.com/recaptcha/. Uživatel si může vybrat, že místo zadávání zobrazených písmen mu bude přehrána krátká zpráva obsahující čísla, která slouží jako kód. Pokud nejsou generovány žádné kryptogramy, server je pravděpodobně mimo službu.');
 
 @define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_STYLE', 'Který typ kryptogramů použít?');
 @define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_STYLE_DESC', 'Vyberte jeden z následujících typů: red (červený), white (bílý), blackglass (černé sklo). Tato volba funguje pouze s povoleným javascriptem.');
 
 @define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PUB', 'Veřejný klíč pro kryptogramy Recaptcha');
-@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PUB_DESC', 'Zadejte veřejnou (public) část klíče pro komunikaci se serveren recaptcha.net. O vygenerování páru klíče (veřejný + soukromý klíč) můžete požádat na http://www.recaptcha.net/api/getkey');
+@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PUB_DESC', 'Zadejte veřejnou (public) část klíče pro komunikaci se serveren recaptcha.net. O vygenerování páru klíče (veřejný + soukromý klíč) můžete požádat na https://www.google.com/recaptcha/admin');
 
 @define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PRIV', 'Soukromý klíč recaptcha');
-@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PRIV_DESC', 'Zadejte soukromou (private) část klíče pro komunikaci se serveren recaptcha.net. O vygenerování páru klíče (veřejný + soukromý klíč) můžete požádat na http://www.recaptcha.net/api/getkey');
+@define('PLUGIN_EVENT_RECAPTCHA_RECAPTCHA_PRIV_DESC', 'Zadejte soukromou (private) část klíče pro komunikaci se serveren recaptcha.net. O vygenerování páru klíče (veřejný + soukromý klíč) můžete požádat na https://www.google.com/recaptcha/admin');
 
 @define('PLUGIN_EVENT_RECAPTCHA_CAPTCHAS_TTL', 'Vynutit kryptogramy po uplynutí kolika dní?');
 @define('PLUGIN_EVENT_RECAPTCHA_CAPTCHAS_TTL_DESC', 'Použití kryptogramů může být vynuceno v závislosti na stáří článků. Zadejte počet dní, po jejichž uplynutí od vydání článku je třeba zadat kryptogram. Hodnota 0 znamená, že kryptogramy budou použity vždy.');
@@ -42,5 +42,5 @@
 @define('PLUGIN_EVENT_RECAPTCHA_ERROR_CAPTCHAS', 'Nezadal(a) jsi správný řetězec podle antispamového obrázku. Podívej se na kryptogram prosím ještě jednou a zadej správné hodnoty.');
 @define('PLUGIN_EVENT_RECAPTCHA_ERROR_RECAPTCHA', 'Nezadali jste veřejný/soukromý klíč v nastavení kryptogramů recaptcha. Kryptogramy budou vypnuty. Pokud je chcete používat, zadejte prosím oba dva klíče v nastavení pluginu Recaptcha, nebo použijte obyčejné kryptogramy (plugin "antispamové metody").');
 
-@define('PLUGIN_EVENT_RECAPTCHA_INFO1', 'Recaptcha je zvláštní druh <a href="http://en.wikipedia.com/wiki/Captcha">kryptogramu</a>. Uživatel musí rozpoznat dvě slova. První systémem výzva-odpověď (ochrana před spamem), a druhé, které pomáhá při digitalizaci knih. Navíc zrakově postižení lidé mohou použít audio-kryptogram. Pro více informací se podívejte na stránku <a href="http://www.recaptcha.net">www.recaptcha.net</a>.<br/>Pamatujte, že abyste mohli používat tento plugin, musíte se registrovat na zmíněné webové stránce.O klíč můžete požádat  <a href="http://www.recaptcha.net/api/getkey?app=serendipity&domain=');
+@define('PLUGIN_EVENT_RECAPTCHA_INFO1', 'Recaptcha je zvláštní druh <a href="http://en.wikipedia.com/wiki/Captcha">kryptogramu</a>. Uživatel musí rozpoznat dvě slova. První systémem výzva-odpověď (ochrana před spamem), a druhé, které pomáhá při digitalizaci knih. Navíc zrakově postižení lidé mohou použít audio-kryptogram. Pro více informací se podívejte na stránku <a href="https://www.google.com/recaptcha/">https://www.google.com/recaptcha/</a>.<br/>Pamatujte, že abyste mohli používat tento plugin, musíte se registrovat na zmíněné webové stránce.O klíč můžete požádat <a href="https://www.google.com/recaptcha/admin');
 @define('PLUGIN_EVENT_RECAPTCHA_INFO2', '">tady</a>. <br/> Pamatujte také prosím, že tento plugin se bude při každém komentáři dotazovat serveru recaptcha, a může proto zpomalit načítání stránek. Pokud bude server recaptcha vypnutý, pak nebudou použity žádné kryptogramy.');

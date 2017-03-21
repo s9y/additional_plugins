326c8dabae8e4b08a8aab7128537a86228e77b52
Matthias Mees <mm@yellowled.de>
2017-03-21 11:10:37 +0100

    Upgradate jQuery to 3.2.1
    The previous update failed because the files for jQuery 3.2.0 were
    not copied from node_modules to the src dirs. I need to write a
    build task for that. :(

--- homepage/index.php
+++ homepage/index.php
@@ -194,7 +194,7 @@ if (is_dir('homepage')) {
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.2.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script>window.jQuery || document.write('<script src="homepage/scripts/jquery/dist/jquery.min.js"><\/script>')</script>
     <script src="homepage/scripts/master.js"></script>
     <!-- Google Analytics -->

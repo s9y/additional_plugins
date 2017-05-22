3f67f1ab30275c1237648ed407d3d81acf8c2e51
Thomas Hochstein <thh@inter.net>
2017-05-22 20:28:26 +0200

    Update dejure-plugin to version 1.5 from upstream. (#57)
    v1.5 taken from <https://dejure.org/vernetzung.html>
    with author's consent.
    
    Some minor changes to the database queries, trying
    to be more compatible to other database backends,
    see <https://board.s9y.org/viewtopic.php?f=10&t=21255>.
    
    Update language files (as far as I speak those
    languages).
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_dejure/UTF-8/lang_de.inc.php
+++ serendipity_event_dejure/UTF-8/lang_de.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-@define('DEJURE_TITLE', 'Dejure.org Rechtvernetzung');
+@define('DEJURE_TITLE', 'dejure.org Rechtsvernetzung');
 @define('DEJURE_DESCRIPTION', 'Verlinkt automatisch zitierte Gesetze und Rechtsprechung (Aktenzeichen und Fundstellen) mit den Inhalten von dejure.org.');
 @define('DEJURE_MAIL', 'E-Mail Adresse des Blogbetreibers');
 @define('DEJURE_MAIL_DESC', 'Diese Informationen werden im Rahmen der Serverkommunikation jeweils an dejure.org übertragen und dienen im übrigen nur der Kommunikation bei technischen Problemen. Die Weitergabe der Daten an Dritte sowie eine Verwendung für Werbezwecke o.ä. ist ausgeschlossen!');
@@ -14,3 +14,5 @@
 @define('DEJURE_LINKSTYLE_DESC', '');
 @define('DEJURE_LINKSTYLE_SHORT', 'Nur die Nummern der Vorschriften verlinken (Bsp.: § _242_ BGB; §§ _278_, _254_ BGB)');
 @define('DEJURE_LINKSTYLE_WIDE', 'Möglichst weite Verlinkung (Bsp.: _§ 242 BGB_; _§§ 278_, _254 BGB_)');
+@define('DEJURE_CACHE', 'Cache leeren');
+@define('DEJURE_CACHE_DESC', 'Den kompletten Cache beim nächsten Aufruf des Blogs leeren. Notwendig, wenn Sie das Linkziel, die CSS-Klasse oder den Linkstil ändern.');

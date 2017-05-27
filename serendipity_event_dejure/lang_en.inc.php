bfe249838d389dd731a543975324ac035c86c53a
Thomas Hochstein <thh@inter.net>
2017-05-27 13:18:42 +0200

    Reformat and rephrase plugin configuration screen.
    Remove HTML added in 88e58798af05a79543e89aa87705e85a325cc164
    as it was not parsed.
    
    Move link style description to info text.
    
    Improve some phrases.
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_dejure/lang_en.inc.php
+++ serendipity_event_dejure/lang_en.inc.php
@@ -2,19 +2,19 @@
 
 @define('DEJURE_TITLE', 'dejure.org automatic linking');
 @define('DEJURE_DESCRIPTION', 'Automatically links quoted statues and judicature (references and source of information) with contents of dejure.org.');
-@define('DEJURE_MAIL', 'E-Mail address of the blog owner');
-@define('DEJURE_MAIL_DESC', 'This information is sent to dejure.org and only servers for communication means on technical failures. No data is forwarded for marketing reasons or similar!');
+@define('DEJURE_MAIL', 'Email address of blog owner');
+@define('DEJURE_MAIL_DESC', 'This information is sent to dejure.org to provide a contact address when problems arise. No data is used or forwarded for marketing reasons or similar!');
 @define('DEJURE_NEWSLETTER', 'Newsletter');
 @define('DEJURE_NEWSLETTER_DESC', 'I want to subscribe to updates to the dejure.org service.');
 @define('DEJURE_TARGET', 'Open links...');
-@define('DEJURE_TARGET_DESC', 'An empty field opens links in the same target, "_blank" in a new one.');
+@define('DEJURE_TARGET_DESC', 'The "target" attribute of the generated links. Leave empty to open links in the same borwser window or frame, use "_blank" top open them in a new browser window.');
 @define('DEJURE_CSS', 'CSS class for dejure.org links');
 @define('DEJURE_CSS_DESC', '');
 @define('DEJURE_LINKSTYLE', 'Style of dejure.org links');
-@define('DEJURE_LINKSTYLE_DESC', '');
-@define('DEJURE_LINKSTYLE_SHORT', 'Only link numbers of judicature (i.e.: § _242_ BGB; §§ _278_, _254_ BGB)');
-@define('DEJURE_LINKSTYLE_WIDE', 'Use detailed linking (i.e.: _§ 242 BGB_; _§§ 278_, _254 BGB_)');
-@define('DEJURE_BUZER', 'Coverage of statutes');
-@define('DEJURE_BUZER_DESC', 'Link to <a href="https://buzer.de/"><i>buzer.de</i></a> for statutes that are not available at <i>dejure.org</i>.');
+@define('DEJURE_LINKSTYLE_DESC', 'Generated links will span just the article or section number (§ _242_ BGB; §§ _278_, _254_ BGB) or include paragraphs or the abbreviation of the statute (_§ 242 BGB_; _§§ 278_, _254 BGB_).');
+@define('DEJURE_LINKSTYLE_SHORT', 'Only link from article/section numbers');
+@define('DEJURE_LINKSTYLE_WIDE', 'Use wide linking');
+@define('DEJURE_BUZER', 'Also link to buzer.de');
+@define('DEJURE_BUZER_DESC', 'Link to buzer.de for statutes that are not available at dejure.org.');
 @define('DEJURE_CACHE', 'Purge cache');
-@define('DEJURE_CACHE_DESC', 'Completely purge the cache - necessary after changes to link targets ("Open links..."), CSS class, style or coverage.');
+@define('DEJURE_CACHE_DESC', 'Completely purge the cache - necessary after changes to link targets ("Open links..."), CSS class, style or linking to buzer.de.');

87eedab7c8c21edfe0e7e33318027565b2c6b539
Thomas Hochstein <thh@inter.net>
2017-07-23 13:48:26 +0200

    [s9y_event_markdown] Update/change language files.
    * Add an explicit SmartyPants option instead
      of a simple "yes".
    * Change (English and German) language files
      accordingly.
    * Change URLs to https to avoid redirects.
    * Bump version to 1.26
    * Update ChangeLog.
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_markdown/lang_en.inc.php
+++ serendipity_event_markdown/lang_en.inc.php
@@ -9,14 +9,15 @@
 @define('PLUGIN_EVENT_MARKDOWN_NAME', 'Markup: Markdown');
 @define('PLUGIN_EVENT_MARKDOWN_DESC', 'Apply Markdown markup to entry text');
 @define('PLUGIN_EVENT_MARKDOWN_EXTRA_NAME', 'Markup: Markdown Extra');
-@define('PLUGIN_EVENT_MARKDOWN_EXTRA_DESC', 'Apply Markdown Extra markup to entry text (See: http://michelf.ca/projects/php-markdown/extra/)');
-@define('PLUGIN_EVENT_MARKDOWN_TRANSFORM', '<a href="http://daringfireball.net/projects/markdown/syntax">Markdown</a> format allowed');
+@define('PLUGIN_EVENT_MARKDOWN_EXTRA_DESC', 'Apply Markdown Extra markup to entry text (See: https://michelf.ca/projects/php-markdown/extra/)');
+@define('PLUGIN_EVENT_MARKDOWN_TRANSFORM', '<a href="https://daringfireball.net/projects/markdown/syntax">Markdown</a> format allowed');
 
 @define('PLUGIN_EVENT_MARKDOWN_VERSION', 'Markdown Version');
-@define('PLUGIN_EVENT_MARKDOWN_VERSION_BLABLAH', 'Which version of Markdown do you want to use? (http://michelf.ca/projects/php-markdown/ and http://michelf.ca/blog/2013/php-markdown-lib/)');
+@define('PLUGIN_EVENT_MARKDOWN_VERSION_BLABLAH', 'Which version of Markdown do you want to use? (https://michelf.ca/projects/php-markdown/ and https://michelf.ca/blog/2013/php-markdown-lib/)');
 
 @define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_NAME', 'Markup: SmartyPants');
-@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_DESC', 'Apply SmartyPants (+Typographer) markup to entry text (See: http://michelf.ca/projects/php-smartypants/). Use with lib only!');
-@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_EXTENDED', '+Typographer');
-@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_NEVER', 'Disabled');
+@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_DESC', 'Apply SmartyPants (+Typographer) markup to entry text (See: https://michelf.ca/projects/php-smartypants/). Use with lib only!');
+@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS', 'SmartyPants');
+@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_EXTENDED', 'SmartyPants Typographer');
+@define('PLUGIN_EVENT_MARKDOWN_SMARTYPANTS_NEVER', 'none');
 

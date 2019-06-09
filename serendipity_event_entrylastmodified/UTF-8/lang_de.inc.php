afcff99cd0101b0150ae44cd265bdb9c22b4bce1
Thomas Hochstein <thh@inter.net>
2019-06-09 13:48:09 +0200

    Add/enhance German translation.
    Add ChangeLog.
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_entrylastmodified/UTF-8/lang_de.inc.php
+++ serendipity_event_entrylastmodified/UTF-8/lang_de.inc.php
@@ -1,5 +1,15 @@
 <?php # 
 
-        @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_NAME',     'Datum der letzten Aktualisierung eines Eintrags anzeigen');
-        @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_DESC',     'Zeigt das Datum der letzten Aktualisierung eines Eintrags auf der Übersichtsseite des Blogs an.');
-        @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_HTML',     'Zuletzt bearbeitet am');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_NAME',     'Datum der letzten Aktualisierung eines Eintrags anzeigen');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_DESC',     'Zeigt das Datum der letzten Aktualisierung eines Eintrags an.');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_HTML',     'Zuletzt bearbeitet am');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_NOTMODIFIED','Unbearbeitet');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_SHOWNOTMODIFIED','Hinweis bei unbearbeiteten Einträgen anzeigen');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_SHOWNOTMODIFIED_DESC','"Ja" zeigt auch einen Hinweistext bei nicht bearbeiteten Einträgen an; ansonsten wird das Datum der letzten Aktualisierung nur dann angezeigt, wenn der Eintrag mindestens einmal aktualisiert wurde.');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_POSITION','Position des Hinweises.');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_POSITION_DESC','');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_LEFT', 'Links');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_CENTER', 'Mittig');
+    @define('PLUGIN_EVENT_ENTRYLASTMODIFIED_RIGHT', 'Rechts');
+
+?>

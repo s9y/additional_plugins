18359c72b7a901bfa68529339b16b29c706beebf
Thomas Hochstein <thh@inter.net>
2019-08-17 22:53:58 +0200

    [event_layout_printerfriendly] Remove constant.
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- serendipity_event_layout_printerfriendly/lang_de.inc.php
+++ serendipity_event_layout_printerfriendly/lang_de.inc.php
@@ -1,6 +1,5 @@
 <?php # 
 
-        @define('PLUGIN_LAYOUT_PRINTERFRIENDLY_VERSION', '1.0');
         @define('PLUGIN_LAYOUT_PRINTERFRIENDLY_NAME', 'Layout-Plugin: Druckfreundliche Version');
         @define('PLUGIN_LAYOUT_PRINTERFRIENDLY_BLAHBLAH', 'Ermöglicht den besseren Ausdruck eines Eintrages im Browser');
         @define('PLUGIN_LAYOUT_PRINTERFRIENDLY_SIDEBARS', 'Seitenleisten ausdrucken');

6eccfa6802d0e37d8628b723bfe51454ce84129f
surrim <surrim@happyhydro.org>
2021-07-08 17:22:57 +0200

    using serendipity_plugin_api::load_language() for some more plugins

--- serendipity_plugin_twitter/serendipity_event_twitter.php
+++ serendipity_plugin_twitter/serendipity_event_twitter.php
@@ -6,15 +6,9 @@ if (IN_serendipity !== true) {
     die ("Don't hack!");
 }
 
-// Probe for a language include with constants. Still include defines later on, if some constants were missing
-$probelang = dirname(__FILE__) . '/' . $serendipity['charset'] . 'lang_' . $serendipity['lang'] . '.inc.php';
-if (file_exists($probelang)) {
-    include $probelang;
-}
+@serendipity_plugin_api::load_language(dirname(__FILE__));
 
-include dirname(__FILE__) . '/lang_en.inc.php';
-
-include dirname(__FILE__) . '/plugin_version.inc.php';
+include_once dirname(__FILE__) . '/plugin_version.inc.php';
 
 require_once dirname(__FILE__) . '/classes/Twitter.php';
 require_once dirname(__FILE__) . '/classes/TwitterOAuthApi.php';

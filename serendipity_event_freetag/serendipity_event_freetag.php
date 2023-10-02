90ee58f00ee8daaa5b8356ff84a9b6f407a6e3c0
Mario Hommel <mario@hommel-net.de>
2023-10-02 20:47:08 +0200

    Avoid more warnings for PHP8.2

--- serendipity_event_freetag/serendipity_event_freetag.php
+++ serendipity_event_freetag/serendipity_event_freetag.php
@@ -66,7 +66,7 @@ class serendipity_event_freetag extends serendipity_event
             'smarty'      => '2.6.7',
             'php'         => '7.0'
         ));
-        $propbag->add('version',       '3.70.7');
+        $propbag->add('version',       '3.70.8');
         $propbag->add('event_hooks',    array(
             'frontend_fetchentries'                             => true,
             'frontend_fetchentry'                               => true,

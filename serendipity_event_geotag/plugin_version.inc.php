67f261a88f168c135f8ecf88ece94c7d8925e5ee
surrim <root@surrim.org>
2023-10-20 21:05:44 +0100

    Changed my name
    See also https://github.com/s9y/additional_plugins/commit/b2166d36ee626a8189c153fadaf567880541f0a0

--- serendipity_event_geotag/plugin_version.inc.php
+++ serendipity_event_geotag/plugin_version.inc.php
@@ -2,4 +2,4 @@
 
 // Actual version of both plugins
 @define('PLUGIN_EVENT_GEOTAG_VERSION',	'1.36.1');
-@define('PLUGIN_EVENT_GEOTAG_AUTHOR', 	'Zoran Kovacevic, Grischa Brockhaus, Matthias Gutjahr, Martin Sewelies');
+@define('PLUGIN_EVENT_GEOTAG_AUTHOR', 	'Zoran Kovacevic, Grischa Brockhaus, Matthias Gutjahr, Kathi Sewelies');

4b97bf8d926a24c60d9faf5d27271253b9557dc5
surrim <root@surrim.org>
2025-01-19 15:53:41 +0100

    updated openlayers to v10.3.1

--- serendipity_event_osm/ressources/osm.js
+++ serendipity_event_osm/ressources/osm.js
@@ -37,10 +37,8 @@ window.addEventListener("load", () => {
 					const date = new Date(article.date * 1000);
 
 					return new ol.style.Style({
-						image: new ol.style.Circle({
-							radius: 6,
-							fill: new ol.style.Fill({color: dateToColor(date)})
-						})
+						image: new ol.style.Circle({radius: 6}),
+						fill: new ol.style.Fill({color: dateToColor(date)})
 					});
 				},
 				zIndex: Infinity

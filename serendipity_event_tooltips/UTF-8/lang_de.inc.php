546b4f5c5b670d8c439b0b4c5b0a1a0e192604b1
onli <onli@paskuda.biz>
2025-06-30 15:41:20 +0200

    Remove the tooltip plugin As it is broken, likely since https://github.com/s9y/additional_plugins/commit/682831f358d6dc3e891e7074cc51d65e1cf2ad16

--- serendipity_event_tooltips/UTF-8/lang_de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-        @define('PLUGIN_EVENT_TOOLTIPS_NAME', 'Tooltips');
-        @define('PLUGIN_EVENT_TOOLTIPS_DESC', 'Aktiviert Tooltips für Textbereiche und Bilder.');
-        @define('PLUGIN_EVENT_TOOLTIPS_FULLIMAGES_NAME', 'Originalbilder anzeigen');
-        @define('PLUGIN_EVENT_TOOLTIPS_FULLIMAGES_DESC', 'Zeige das Originalbild an, wenn der Benutzer mit der Mouse über das Thumbnail fährt.');
-?>

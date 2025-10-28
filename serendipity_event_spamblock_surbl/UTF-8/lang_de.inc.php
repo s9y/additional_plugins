3d5a1a1aed30e87fe208d3b2c6e7e06a0c76fcdf
onli <onli@paskuda.biz>
2025-10-28 18:49:13 +0100

    surbl: Remove the plugin from spartacus (#207)
    With an always failing if clause, it did not do anything. And the code behind the if also would not work anymore, as DNSBL/SURBL.php relies on a removed online list of two leveled domain endings (like .co.uk). Removing the plugin completely would free the s9y core from carrying Net_DNSBL and Net_DNS2, see https://github.com/s9y/Serendipity/issues/500

--- serendipity_event_spamblock_surbl/UTF-8/lang_de.inc.php
+++
@@ -1,5 +0,0 @@
-<?php # 
-
-   @define('PLUGIN_EVENT_SPAMBLOCKSURBL_TITLE', 'Spamschutz (SURBL)');
-   @define('PLUGIN_EVENT_SPAMBLOCKSURBL_DESC', 'URL abweisen die als SPAM geblacklistet sind.');
-   @define('PLUGIN_EVENT_SPAMBLOCK_ERROR_SURBL', 'Spamschutz: Ihre Nachricht enthält eine URL, die als SPAM geführt wird, daher wird Ihr Kommentar abgewiesen.');

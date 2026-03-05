52ef94b9fa7bfe9aa0da1102e4f1e80b3a6c7358
jmglastetter <17621216+jmglastetter@users.noreply.github.com>
2026-03-05 02:36:05 -0600

    lightbox 3.0: Addition of PhotoSwipe 5 (Vanilla JS) (#209)
    * Addition of PhotoSwipe 5
    
    Bumped version to 3.0.0.
    Integrated PhotoSwipe 5.x vanilla JS library.
    Removed jQuery requirement for PhotoSwipe mode.
    Added aspect-ratio logic to prevent layout shift.
    Implemented root-appended responsive captions.
    
    * Change line endings to original LF, to make diff comparison work
    
    * docuemnt changes (in ChangeLog instead of code file)
    
    * Fix photoswipe not engaging when image had class or was webp/avif
    Format fix also for other lightboxes
    
    * Make photoswipe respect gallery setting
    
    * Restore comments and formatting, update language files
    
    * Re-use init_js config for photoswipe
    
    * Be defensive about existing regex variable
    
    * document changes
    
    ---------
    
    Co-authored-by: onli <onli@paskuda.biz>

--- serendipity_event_lightbox/lang_de.inc.php
+++ serendipity_event_lightbox/lang_de.inc.php
@@ -7,14 +7,14 @@
  */
 
 @define('PLUGIN_EVENT_LIGHTBOX_NAME', 'Lightbox für Blog Einträge mit Bildern');
-@define('PLUGIN_EVENT_LIGHTBOX_DESC', 'Das Lightbox Plugin ist eine einfaches unauffälliges Skript, das Bilder auf der Seite überlagern kann, wenn die große Version angeklickt wurde. Es fügt sich selbstständig in die Seite ein und funktioniert mit jedem modernen Browser. Alle Leuchtkästen verschönern Bilder-Popups. Dieses Plugin durchsucht ihre Einträge und ersetzt jeden auf ein Bild verweisenden \'a href="XXX"\' Link so, dass die interne Darstellung benutzt wird. Wenn Sie also ein Thumbnail Bild mit einem Popup der großen Version haben wollen, so fügen Sie das Thumbnail in ihren Eintrag ein und verlinken dieses mit der großen Version. Um auch versteckte Bilder mit display:none anzuzeigen, nutzen Sie das Lightbox2 Script. Diese Leuchtkasten Scripte sind alle jQuery basiert. Sie unterstützen nicht allein nur Bildtypen, sondern überwiegend auch anderes, wie Ajax, Videos, Flash, YouTube, iFrame, Inline oder modale Felder. Dieses Plugin nutzt sie nur für Fotografie Leuchtkästen, aber Sie können leicht andere Typen ihren Blog-Einträgen manuell hinzufügen und eine lightbox entsprechend für diesen Zweck initiieren, so wie es den jeweiligen Online-Dokumentationen beschrieben wird.');
+@define('PLUGIN_EVENT_LIGHTBOX_DESC', 'Eine Lightbox ist ein unauffälliges Skript, das Bilder auf der Seite in einem Popup anzeigt, wenn die große Version angeklickt wurde. Es fügt sich selbstständig in die Seite ein und funktioniert mit jedem modernen Browser. Dieses Plugin durchsucht ihre Einträge und ersetzt jeden auf ein Bild verweisenden \'a href="XXX"\' Link so, dass die neue Darstellung benutzt wird. Wenn Sie also ein Thumbnail Bild mit einem Popup der großen Version haben wollen, so fügen Sie das Thumbnail in ihren Eintrag ein und verlinken dieses mit der großen Version. Um auch versteckte Bilder mit display:none anzuzeigen, nutzen Sie das Lightbox2 Script. Die Skripts unterstützen nicht allein nur Bildtypen, sondern überwiegend auch anderes, wie Ajax, Videos, Flash, YouTube, iFrame, Inline oder modale Felder. Dieses Plugin nutzt sie nur für Bilder, aber Sie können andere Typen ihren Blog-Einträgen manuell hinzufügen und eine Lightbox entsprechend für diesen Zweck initiieren, so wie es den jeweiligen Online-Dokumentationen beschrieben wird.');
 @define('PLUGIN_EVENT_LIGHTBOX_TYPE', 'Wählen Sie das Skript aus, das ihre Bilder/Links formatieren soll');
 @define('PLUGIN_EVENT_LIGHTBOX_PATH', 'Pfad zu den Skripten');
-@define('PLUGIN_EVENT_LIGHTBOX_PATH_DESC', 'Geben Sie hier den kompletten HTTP Pfad ein (alles nach ihrem Domain Namen), der das Verzeichnis des Plugins angibt.');
+@define('PLUGIN_EVENT_LIGHTBOX_PATH_DESC', 'Geben Sie hier den kompletten HTTP-Pfad ein (alles nach ihrem Domain Namen), der das Verzeichnis des Plugins angibt.');
 
-@define('PLUGIN_EVENT_LIGHTBOX_OPTIMIZATION', 'JavaScript Ladeoptimierung');
-@define('PLUGIN_EVENT_LIGHTBOX_OPTIMIZATION_DESC', 'Grundsätzlich werden Skripte und CSS Dateien von Lightbox immer geladen. Setzen Sie diese Option auf "Ja", wenn Lightbox nur geladen werden soll, wenn sich auch wirklich Bilder auf der aktuellen Seite befinden. Dies kann die Ladezeit von Seiten ohne Bilder spürbar verkürzen.');
+@define('PLUGIN_EVENT_LIGHTBOX_OPTIMIZATION', 'JavaScript-Ladeoptimierung');
+@define('PLUGIN_EVENT_LIGHTBOX_OPTIMIZATION_DESC', 'Grundsätzlich werden Skripte und CSS-Dateien von Lightbox immer geladen. Setzen Sie diese Option auf "Ja", wenn Lightbox nur geladen werden soll, wenn sich auch wirklich Bilder auf der aktuellen Seite befinden. Dies kann die Ladezeit von Seiten ohne Bilder spürbar verkürzen.');
 @define('PLUGIN_EVENT_LIGHTBOX_GALLERY', 'Galerie-Erzeugung');
 @define('PLUGIN_EVENT_LIGHTBOX_GALLERY_NONE', 'Nur einzelnes Bild');
-@define('PLUGIN_EVENT_LIGHTBOX_GALLERY_ENTRY', 'nur Fotos eines Artikels');
-@define('PLUGIN_EVENT_LIGHTBOX_GALLERY_PAGE', 'alle Fotos der Seite');
+@define('PLUGIN_EVENT_LIGHTBOX_GALLERY_ENTRY', 'Nur Fotos eines Artikels');
+@define('PLUGIN_EVENT_LIGHTBOX_GALLERY_PAGE', 'Alle Fotos der Seite');
--- serendipity_event_lightbox/lang_en.inc.php
+++ serendipity_event_lightbox/lang_en.inc.php
@@ -7,7 +7,7 @@
  */
 
 @define('PLUGIN_EVENT_LIGHTBOX_NAME', 'Lightbox for entry images');
-@define('PLUGIN_EVENT_LIGHTBOX_DESC', 'Lightbox JS is a simple, unobtrusive script used to overlay images on the current page. It\'s a snap to setup and works with all modern browsers. All lightboxes pop-up images. This plugin searches through your entries, and replaces every image \'a href="XXX"\' link to use the internal display. So if you want your thumbnail images to popup large, you need to insert your images as links to the large version. To display also hidden set images by display:none, use lightbox2. These lightbox scripts are all jQuery based. They do not only support image types, they may also add support for ajax, videos, flash, YouTube, iFrame, inline, or modal boxes. This Plugin uses them for image lightboxes only, but you can easily add other content types manually to your entries and start the chosen lightbox as described in their online documentaries.');
+@define('PLUGIN_EVENT_LIGHTBOX_DESC', 'Lightboxes are unobtrusive scripts used to show images in an overlay when clicked. It\'s a snap to setup and works with all modern browsers. All lightboxes pop-up images instead of showing them directly. This plugin searches through your entries, and replaces every image \'a href="XXX"\' link to use the new display method. So if you want your thumbnail images to popup large, you need to insert your images as links to the large version. To display also hidden set images by display:none, use lightbox2. Those scripts do not only support images, they may also have support for ajax, videos, flash, YouTube, iFrame, inline, or modal boxes. This Plugin uses them for images only, but you can add other content types manually to your entries and start the chosen lightbox as described in their online documentaries.');
 @define('PLUGIN_EVENT_LIGHTBOX_TYPE', 'Select the script to pretty-format your links/images');
 @define('PLUGIN_EVENT_LIGHTBOX_PATH', 'Path to the scripts');
 @define('PLUGIN_EVENT_LIGHTBOX_PATH_DESC', 'Enter the full HTTP path (everything after your domain name) that leads to this plugin\'s directory.');
@@ -20,6 +20,6 @@
 @define('PLUGIN_EVENT_LIGHTBOX_GALLERY_PAGE', 'Gallery with photos of the page');
 
 @define('PLUGIN_EVENT_LIGHTBOX_INIT_JS', 'Initial JavaScript configuration object');
-@define('PLUGIN_EVENT_LIGHTBOX_INIT_JS_DESC', 'Some lightbox types allow to pass custom configuration objects, so you can enter "{social_tools: false}" for example. Currently works with prettyPhoto only.');
+@define('PLUGIN_EVENT_LIGHTBOX_INIT_JS_DESC', 'Some lightbox types allow to pass custom configuration objects, so you can enter "{social_tools: false}" for example.');
 
 
